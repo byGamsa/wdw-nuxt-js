f947d008e4ae2e5d9fe74e738e528c1769b2efa4
byGamsa <larsgerigk@gmx.de>
2025-11-09 20:16:31 +0100

    feat: component for postlist

---
+++ exercises/07-components-and-props/solutions/src/components/PostList.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { defineProps, defineEmits } from 'vue'
+
+    const props = defineProps({
+        posts: {
+            type: Array,
+            required: true
+        }
+    })
+
+    const emit = defineEmits(['select-post'])
+
+    function handleOpen(post) {
+        emit('select-post', post)
+    }
+
+</script>
+
+<template>
+    <div class="posts">
+        <p v-if="posts.length === 0">Noch keine Beiträge vorhanden</p>
+        <div v-else class="post-list">
+            <div v-for="post in posts" :key="post.id" class="post-card" @click="handleOpen(post)">
+                <img :src="post.imageUrl" :alt="post.title"/>
+                <div class="post-info">
+                    <span class="title">{{ post.title }}</span>
+                    <span class="likes">{{ post.likes }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
